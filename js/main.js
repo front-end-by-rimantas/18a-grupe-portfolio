b613a6d3a2e2407ff4da0ce3342e8fea83d54010
Rimantas <belauzas@yahoo.com>
2020-10-08 11:42:54 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// header
+// hero
+// about us
+// portfolio gallery
+// our services
+// our team
+// achievements
+// pricing
+// blog
+// testimonials
+// contact us
+// maps
+// footer
+
+// header
+// hero
+// about us
+// portfolio gallery
+// our services
+// our team
+// achievements
+// pricing
+// blog
+// testimonials
+// contact us
+// maps
+// footer
